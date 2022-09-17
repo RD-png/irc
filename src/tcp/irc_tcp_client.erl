5dc106c0a91cd2e94d2b4ffb60f2cade76217023
Ryan Denby <ryan.543@live.co.uk>
2022-09-17 19:32:07 +0100

    Change irc_tcp_client to gen_server process

--- src/tcp/irc_tcp_client.erl
+++ src/tcp/irc_tcp_client.erl
@@ -8,6 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(irc_tcp_client).
 
+-behaviour(gen_server).
 -behaviour(ranch_protocol).
 
 -include("irc.hrl").
@@ -16,49 +17,70 @@
 %% API
 -export([start_link/3]).
 
-%% ranch_protocol callbacks
--export([init/3]).
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
--record(state, {id       :: client_id(),
-                protocol :: tcp_connection()}).
+-record(state, {id        :: client_id(),
+                transport :: module(),
+                protocol  :: tcp_connection()}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
 start_link(Ref, Transport, Opts) ->
-  Pid = spawn_link(?MODULE, init, [Ref, Transport, Opts]),
-  {ok, Pid}.
+  {ok, proc_lib:spawn_link(?MODULE, init, [{Ref, Transport, Opts}])}.
 
 %%%===================================================================
-%%% ranch_protocol callbacks
+%%% gen_server callbacks
 %%%===================================================================
 
-init(Ref, Transport, _Opts = []) ->
+init({Ref, Transport, _Opts = []}) ->
   {ok, Socket} = ranch:handshake(Ref),
+  ok = Transport:setopts(Socket, [{active, once}]),
   ClientID = irc_client:register(Socket),
-  loop(Socket, Transport, #state{id = ClientID, protocol = Socket}).
+  gen_server:enter_loop(?MODULE, [], #state{id        = ClientID,
+                                            transport = Transport,
+                                            protocol  = Socket}).
+
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+handle_info({tcp, _Socket, <<"quit!", _Rest/binary>>}, State) ->
+  {stop, normal, State};
+handle_info({tcp, Socket, Packet}, #state{id        = ClientID,
+                                          transport = Transport} = State) ->
+  Transport:setopts(Socket, [{active, once}]),
+  case irc_socket_client:handle(Packet, ClientID) of
+    ok ->
+      ok;
+    Response ->
+      Transport:send(Socket, Response)
+  end,
+  {noreply, State, ?SOCKET_TIMEOUT};
+handle_info(timeout, #state{transport = Transport,
+                            protocol  = Socket} = State) ->
+  Transport:send(Socket, ?TIMEOUT_MSG),
+  {stop, normal, State};
+handle_info(Info, State) ->
+  lager:error("Received unhandled message ~p", [Info]),
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-loop(Socket, Transport, #state{id = ClientID} = State) ->
-  case Transport:recv(Socket, 0, ?SOCKET_TIMEOUT) of
-    {ok, <<"quit!", _Rest/binary>>} ->
-      Transport:send(Socket, ?QUIT_MSG);
-    {ok, Packet} ->
-      case irc_socket_client:handle(Packet, ClientID) of
-        ok ->
-          ok;
-        Response ->
-          Transport:send(Socket, Response)
-      end,
-      loop(Socket, Transport, State);
-    {error, timeout} ->
-      Transport:send(Socket, ?TIMEOUT_MSG);
-    {error, Reason} ->
-      lager:info("TCP Client '~p' exited with reason '~p'", [ClientID, Reason])
-  end,
-  irc_client:unregister(ClientID),
-  ok = Transport:close(Socket).
